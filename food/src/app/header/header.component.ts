8983047671ae630c3bbddebc5cb4a448017d8cbe
MOHD ADIL <119355639+mohdadil12345@users.noreply.github.com>
2024-05-14 22:53:02 +0530

    folder

---
+++ food/src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+
+ api = 'https://ride-share-backend-pw1v.onrender.com'
+
+}
